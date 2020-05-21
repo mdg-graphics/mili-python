9fe75798f079b8b4e801977c1c656a18d6dd2719
James K. Durrenberger <durrenberger1@llnl.gov>
2020-05-21 11:33:51 -0700

    Added license info to code

--- mili_reader_test.py
+++ mili_reader_test.py
@@ -1,3 +1,29 @@
+"""
+Copyright (c) 2016, Lawrence Livermore National Security, LLC. 
+ Produced at the Lawrence Livermore National Laboratory. Written 
+ by Kevin Durrenberger: durrenberger1@llnl.gov. CODE-OCEC-16-056. 
+ All rights reserved.
+
+ This file is part of Mili. For details, see <URL describing code 
+ and how to download source>.
+
+ Please also read this link-- Our Notice and GNU Lesser General 
+ Public License.
+
+ This program is free software; you can redistribute it and/or modify 
+ it under the terms of the GNU General Public License (as published by 
+ the Free Software Foundation) version 2.1 dated February 1999.
+
+ This program is distributed in the hope that it will be useful, but 
+ WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF 
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms 
+ and conditions of the GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License 
+ along with this program; if not, write to the Free Software Foundation, 
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+"""
 import unittest
 import mili_reader_lib as reader
 import os, sys
